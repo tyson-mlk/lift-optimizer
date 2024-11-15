5f0d52c7f0b050a10613754fd3a3d83fe37cab7c
tyson-mlk <tanyeesern@gmail.com>
2024-11-15 11:17:42 +0800

    auto create logs directory if not exists

--- src/utils/Logging.py
+++ src/utils/Logging.py
@@ -1,10 +1,28 @@
 import logging
+import os, errno
 
-def get_logger(logger_filename, logging_level):
-    py_logger = logging.getLogger(logger_filename)
+def get_logger(logger_name, logging_level):
+
+    logfile_dir = '../logs'
+    if not os.path.exists(logfile_dir):
+        try:
+            os.makedirs(logfile_dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    logfile_name = f"{logfile_dir}/{logger_name}.log"
+    if not os.path.exists(logfile_name):
+        try:
+            open(logfile_name, 'a').close()
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    py_logger = logging.getLogger(logfile_name)
     py_logger.setLevel(logging_level)
 
-    py_handler = logging.FileHandler(f"../logs/{logger_filename}.log")
+    py_handler = logging.FileHandler(logfile_name)
     py_formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
     py_handler.setFormatter(py_formatter)
