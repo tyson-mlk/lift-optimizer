2678978557ab6eec828f5461b6cdb8f81c16f133
tyson-mlk <tanyeesern@gmail.com>
2024-12-17 08:50:04 +0800

    add test cases for multiple lifts

--- tests/test_multiple_lift.py
+++ tests/test_multiple_lift.py
@@ -16,6 +16,14 @@ async def job_arrival():
     print('second passenger arrival', datetime.now())
     await passenger_arrival('001', '003', datetime.now())
     print('third passenger arrival', datetime.now())
+    await asyncio.sleep(9)
+    await passenger_arrival('003', '001', datetime.now())
+    await passenger_arrival('005', '006', datetime.now())
+    await asyncio.sleep(4)
+    await passenger_arrival('001', '000', datetime.now())
+    await asyncio.sleep(5)
+    await passenger_arrival('005', '006', datetime.now())
+
 
 # simulates run of multiple continuous exponential processes in fixed time
 async def all_arrivals():
@@ -38,18 +46,22 @@ async def lift_operation():
 
 async def track():
     await asyncio.sleep(0)
-    l1 = PASSENGERS.tracking_lifts[0]
     l2 = PASSENGERS.tracking_lifts[1]
     await asyncio.sleep(1.2)
-    print(PASSENGERS.df)
+    # test that passengers are assigned relating to pre-existing assignments
     assert (PASSENGERS.df.loc[[1,2,3],'lift'].values == ['L1', 'L2', 'L1']).all()
-    await asyncio.sleep(8)
-    print('lifts')
-    print(l1.passengers.df)
-    print(l2.passengers.df)
+    await asyncio.sleep(11)
+    assert PASSENGERS.df.loc[5,'lift'] == 'L2'
+    await asyncio.sleep(2)
+    # test for lift turning when all further passengers are assigned
+    assert PASSENGERS.df.loc[4,'lift'] == 'L1'
+    await asyncio.sleep(12)
+    # test for passenger 7 served by L2 after L1 served all remaining
+    assert 7 in l2.passengers.df.index
+    
 
 async def main():
-    timeout = 10
+    timeout = 27
     start_time = datetime.now()
     start_time.hour
     try:
